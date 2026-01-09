cde37a72d39479cb8fc8c28ba826f9ce228a4767
Sahil24-07-05 <sabarwalsahil24@gmail.com>
2026-01-09 19:35:31 +0530

    Updated contact details

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -3,9 +3,9 @@ export default function Contact() {
     <section id="contact" className="py-20 scroll-mt-24">
       <h2 className="text-2xl font-semibold mb-4">Contact</h2>
       <p className="text-slate-400">
-        📧 sahilsabarwal@example.com <br />
-        💼 LinkedIn: linkedin.com/in/sahil-sabarwal <br />
-        🧑‍💻 GitHub: github.com/sahilsabarwal
+        📧 sahilsabarwal24@gmail.com <br />
+        💼 LinkedIn: https://www.linkedin.com/in/sahil-sabarwal24/ <br />
+        🧑‍💻 GitHub: https://github.com/Sahil24-07-05
       </p>
     </section>
   );
